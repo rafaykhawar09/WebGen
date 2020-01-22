74f24351ce2bf3d3e74243d909828533d6dfd091
Mr. A <ag.perch@gmail.com>
2020-01-22 12:44:08 -0800

    html routes added

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+
+var path = require("path");
+
+module.exports = function(app) {
+
+  
+
+  // index route loads index.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
+  });
+
+  // about route loads about.html
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/about.html"));
+  });
+
+
+};
