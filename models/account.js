bcb72596edf7073450eb7e99355b6d4273c54a45
Mr. A <ag.perch@gmail.com>
2020-01-22 14:23:04 -0800

    updatetd the tables

--- models/account.js
+++ models/account.js
@@ -1,13 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
-    var Account = sequelize.define("Account", {
-      account_owner:{ type: DataTypes.STRING, allowNull: false},
-      business_type:{
-        type: DataTypes.STRING,
-        defaultValue: "Restaurant"
-      },
-      company_name:{ type: DataTypes.STRING, allowNull: false},
-      route:DataTypes.STRING
-    });
-    return Account;
-  };
+  var Account = sequelize.define("Account", {
+    account_owner:{ type: DataTypes.STRING, allowNull: false},
+    business_type:{
+      type: DataTypes.STRING,
+      defaultValue: "Restaurant"
+    },
+    company_name:{ type: DataTypes.STRING, allowNull: false},
+    phone:{ type: DataTypes.INTEGER, allowNull: false},
+    route:DataTypes.STRING
+  });
+
   
+  return Account;
+};
--- models/privilege.js
+++ models/privilege.js
@@ -1,13 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
-    var Privilege = sequelize.define("Privilege", {
-      priv_name:{ type: DataTypes.STRING, allowNull: false},
-      user:DataTypes.BOOLEAN,
-      web:DataTypes.BOOLEAN,
-      menu:DataTypes.BOOLEAN,
-      menu_print:DataTypes.BOOLEAN,
-      schedule:DataTypes.BOOLEAN,
-      reservation:DataTypes.BOOLEAN
+  var Privilege = sequelize.define("Privilege", {
+    priv_name:{ type: DataTypes.STRING, allowNull: false},
+    manage_user:DataTypes.BOOLEAN,
+    design_web:DataTypes.BOOLEAN,
+    manage_menu:DataTypes.BOOLEAN,
+    view_print_menu:DataTypes.BOOLEAN,
+    setup_reservation:DataTypes.BOOLEAN,
+    view_reservation:DataTypes.BOOLEAN
+  });
+
+  Privilege.associate = function(models) {
+    // We're saying that a Privilege should belong to an Author
+    // A Privilege can't be created without an Author due to the foreign key constraint
+    Privilege.belongsTo(models.Role, {
+      foreignKey: {
+        allowNull: false
+      }
     });
-    return Privilege;
   };
-  
+
+  return Privilege;
+};
