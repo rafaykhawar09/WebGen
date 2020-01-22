c111d9e979193bb76698e0faf3c4457689edef38
Mr. A <ag.perch@gmail.com>
2020-01-22 15:15:49 -0800

    changed the html route

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,19 +1,18 @@
 
 var path = require("path");
+var express = require("express")
+var router = express.Router()
 
-module.exports = function(app) {
-
-  
 
   // index route loads index.html
-  app.get("/", function(req, res) {
+  router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/assets/html/index.html"));
   });
 
   // about route loads about.html
-  app.get("/about", function(req, res) {
+  router.get("/about", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/html/about.html"));
   });
 
 
-};
+module.exports = router;
