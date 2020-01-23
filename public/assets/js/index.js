1fade55b5d9fd27032ae42900b4aa7fee57f1044
Rafay Khawar <rafaykhawar09@gmail.com>
2020-01-23 14:40:35 -0800

    popup modal, added customer page

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,39 +1,55 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//insert form id
-var accountForm = $("#my-account-form");
+  //insert form id
+  var accountForm = $("#my-account-form");
 
-//form inputs
-var nameInput = $("#name");
-var emailInput = $("#email");
-var passwordInput = $("#password");
-var password2Input = $("#password2");
-var phoneInput = $("#phone");
-var companyInput = $("#company");
+  //form inputs
+  var nameInput = $("#name");
+  var emailInput = $("#email");
+  var passwordInput = $("#password");
+  var password2Input = $("#password2");
+  var phoneInput = $("#phone");
+  var companyInput = $("#company");
 
-$(accountForm).on("submit", function handleFormSubmit(event) {
-        event.preventDefault();
-        // Wont submit the post if we are missing any fields
-        if (!nameInput.val().trim() || !emailInput.val().trim() || !passwordInput.val().trim() || !password2Input.val().trim() || !phoneInput.val().trim() || !companyInput.val().trim() || passwordInput != password2Input) {
-          return;
-        }
-        // Constructing a newPost object to hand to the database
-        var newSubmission = {
-          name: nameInput.val().trim(),
-          email: emailInput.val().trim(),
-          password: passwordInput.val().trim(),
-          phone: phoneInput.val().trim(),
-          company: companyInput.val().trim()
-        };
-        console.log(newSubmission);
-        
-        submitForm(newSubmission);
+  $(accountForm).on("submit", function handleFormSubmit(event) {
+    event.preventDefault();
+    // Wont submit the post if we are missing any fields
+    if (!nameInput.val().trim() || !emailInput.val().trim() || !passwordInput.val().trim() || !password2Input.val().trim() || !phoneInput.val().trim() || !companyInput.val().trim() || passwordInput != password2Input) {
+      return;
+    }
+    // Constructing a newPost object to hand to the database
+    var newSubmission = {
+      name: nameInput.val().trim(),
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+      phone: phoneInput.val().trim(),
+      company: companyInput.val().trim()
+    };
+    console.log(newSubmission);
 
-})
+    submitForm(newSubmission);
 
-function submitForm(form) {
-    $.post("/createuser", form, function() {
+  })
+
+  function submitForm(form) {
+    $.post("/createuser", form, function () {
       window.location.href = "/";
     });
   }
+
+  const modal = $(".my-modal");
+  modal.click(event => {
+    event.stopPropagation();
+  })
+  $(window).click((event) => {
+
+    if (event.target.id === "create-account-btn") {
+
+      if (modal.hasClass("hidden")) {
+        modal.removeClass("hidden");
+      }
+    }
+    else
+      modal.addClass("hidden");
+  });
 });
