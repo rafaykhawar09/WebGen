db985abafbbf4f727dadd547eec6e304288bbc6a
Rafay Khawar <rafaykhawar09@gmail.com>
2020-01-24 13:01:02 -0800

    adding a sticky nav

--- public/assets/js/res-scripts.js
+++ public/assets/js/res-scripts.js
@@ -3,6 +3,8 @@ var hamburger = $(".hamburger");
 var navGroup = $(".nav-group");
 var navInner = $(".navbar-inner")
 var navLinks = $(".nav-link");
+var rightIcon = $(".fa-caret-right");
+var leftIcon = $(".fa-caret-left");
 
 hamburger.on("click", function(){
      
@@ -22,4 +24,17 @@ navLinks.on("click", function(){
 
      if($(window).width() < 1000)
           navGroup.slideToggle("slow");
+});
+
+$(".sticky-menu").on("click", ()=>{
+     $(".sticky-nav-group-wrapper").toggle("slide");
+
+     if(rightIcon.hasClass("hidden")){
+          rightIcon.removeClass("hidden");
+          leftIcon.addClass("hidden");
+     }
+     else{
+          rightIcon.addClass("hidden");
+          leftIcon.removeClass("hidden");
+     }
 });
