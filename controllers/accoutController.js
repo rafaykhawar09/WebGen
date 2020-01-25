44d3f0fd205d297a1bfec7d4da43b087cd79c366
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-01-25 15:00:55 -0800

    Closes #30 add seed data when creating account.

--- controllers/accoutController.js
+++ controllers/accoutController.js
@@ -4,7 +4,7 @@ var db = require("../models");
 
 
 
-router.post("/createuser", function(req, res) {
+router.post("/createaccount", function(req, res) {
     console.log(req.body);
     
 
@@ -24,10 +24,55 @@ router.post("/createuser", function(req, res) {
             AccountId:data.dataValues.id,
             RoleId:1
           }).then(function(data){
-            res.send(data)
+            db.Web_content.create({
+              description: "Welcome to my restaurant, where you should never go home hungry!",
+              our_story_text: "We began in my father's garage after I graduated from culinary school",
+              AccountId:data.dataValues.id
+          }).then(function(data){
+          db.Hours.create({
+                start_day: "Monday",
+                end_day: "Sunday",
+                open: "11:00 am",
+                close: "10:00 pm",
+                AccountId:data.dataValues.id
+            }).then(function(data){
+              db.Menu.bulkCreate([{
+                    menu_item: "Prime Ribeye",
+                    item_description: "Boneless ribeye grilled to perfection.",
+                    price: 55,
+                    optional_price: 0,
+                    gluten_free_ind: false,
+                    vegan_ind: false,
+                    vegetarian_ind: false,
+                    AccountId:data.dataValues.id,
+                    MenuCategoryId: 4,
+                    MenuSubCategoryId: 8},
+                {   menu_item: "Denver Omelette",
+                    item_description: "Three egg omelette with ham, bacon, tomatoes & bell peppers",
+                    price: 13,
+                    optional_price: 0,
+                    gluten_free_ind: false,
+                    vegan_ind: false,
+                    vegetarian_ind: false,
+                    AccountId:data.dataValues.id,
+                    MenuCategoryId: 2,
+                    MenuSubCategoryId: 2},
+                {   menu_item: "Deluxe Burger",
+                    item_description: "One third pound of our special ground beef blend topped with bacon and Tilamook cheddar.",
+                    price: 9.50,
+                    optional_price: 0,
+                    gluten_free_ind: false,
+                    vegan_ind: false,
+                    vegetarian_ind: false,
+                    AccountId:data.dataValues.id,
+                    MenuCategoryId: 3,
+                    MenuSubCategoryId: 2}
+              ]).then(function (data) {
+              res.send(data)
           })
-          
-       
+        })
+          })    
+        })
       }).catch(function(err){
           console.log(err);
           if (err)
