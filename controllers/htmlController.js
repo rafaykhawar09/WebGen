307d3babb3eb996e4ce81d5ee30cae64acaf4f7f
Mr. A <ag.perch@gmail.com>
2020-01-24 16:41:43 -0800

    routes are set

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -4,25 +4,20 @@ var router = express.Router();
 var db = require("../models");
 
 
-  // index route loads index.html
   router.get("/", function(req, res) {
-    db.Web_content.findOne({raw:true}).then(function(response){
-      // res.json(response);
-      res.render("index", response);
-    })
-    // res.sendFile(path.join(__dirname, "../public/assets/html/index.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/html/index.html"));
   });
-  // router.get("/", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/assets/html/index.html"));
-  // });
-
-  // about route loads about.html
+  
   router.get("/about", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/assets/html/about.html"));
   });
-  router.get("/:bizName", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/assets/html/res-home.html"));
-  });
+  
+    router.get("/:bizName", function(req, res) {
+      db.Web_content.findOne({raw:true}).then(function(response){
+        res.render("index", response);
+      })
+    });
+
 
 
 module.exports = router;
